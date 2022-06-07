0d426d8d6efbb945b1ccaf4e3ad2cdf2fce1c1e9
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-07 12:38:00 -0500

    Add: task wait n

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Take the code from wait_n and alter it into a new function task_wait_n.
+    sThe code is nearly identical to wait_n except task_wait_random is being called.
+"""
+import asyncio
+import random
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ Wait_n """
+    times: List[float] = []
+    for i in range(n):
+        times.append(task_wait_random(max_delay))
+    return [await i for i in asyncio.as_completed(times)]
