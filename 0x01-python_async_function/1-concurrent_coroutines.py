21a1b223bbc96a08647538e9782178b90dccadb4
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-07 12:24:31 -0500

    Add: updates

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,12 +9,14 @@
     using sort() because of concurrency.
 """
 import asyncio
+import random
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Wait_n """
-    times: list[float] = []
+    times: List[float] = []
     for i in range(n):
         times.append(wait_random(max_delay))
     return [await i for i in asyncio.as_completed(times)]
