cab56df873f7a657c21ade656a5e5406e23f8a25
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-08 12:59:38 -0500

    Update: update

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Take the code from wait_n and alter
     it into a new function task_wait_n.
-    sThe code is nearly identical to wait_n
+    The code is nearly identical to wait_n
     except task_wait_random is being called.
 """
 import asyncio
