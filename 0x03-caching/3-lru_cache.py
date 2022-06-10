091440e245e7b80cc9711b716cde42d3fb4ecd46
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-10 15:09:36 -0500

    Add: LRU

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" Create a class LRUCache """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRU Cache System """
+    cache_copy = {}
+    insert = 0
+    LRU = 0
+    def __init__(self) -> None:
+        """ Call super and init super """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Must assign to the dictionary the item value for the key"""
+        if key and item:
+            if key in list(self.cache_data.keys()):
+                self.cache_data.update({key: item})
+                self.LRU = self.cache_copy.get(key)[1] + 1
+                self.cache_copy.update({key: [item, self.insert]})
+                self.insert += 1
+            else:
+                if len(self.cache_data.keys()) == self.MAX_ITEMS:
+                    for k, v in self.cache_copy.items():
+                        if v[1] == self.LRU:
+                            print("DISCARD: {}".format(k))
+                            del self.cache_data[k]
+                            del self.cache_copy[k]
+                            self.LRU += 1
+                            break
+                self.cache_copy[key] = [item, self.insert]
+                self.cache_data[key] = item
+                self.insert += 1
+
+    def get(self, key):
+            """ Must return the value in self.cache_data linked to key """
+            if key:
+                try:
+                    return self.cache_data.get(key)
+                except KeyError:
+                    return None
+            return None
