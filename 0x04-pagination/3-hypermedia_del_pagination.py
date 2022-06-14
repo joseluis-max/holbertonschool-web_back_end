37690c3c8b08b1e3014227a257e47e6b84ca1d00
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-14 15:52:47 -0500

    Update: data

--- 0x04-pagination/3-hypermedia_del_pagination.py
+++ 0x04-pagination/3-hypermedia_del_pagination.py
@@ -56,7 +56,7 @@ class Server:
             next_index += 1
         return {
             'index': index,
-            'next_index': next_index,
-            'page_size': page_size,
             'data': data,
+            'page_size': page_size,
+            'next_index': next_index,
         }
