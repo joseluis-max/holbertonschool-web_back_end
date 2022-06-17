724a563d9185ef009a86ea9543a77281f5a2352c
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-17 09:36:27 -0500

    Add: filter datum

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Filtered logger """
+from typing import List
+
+def filter_datum(fields: List[str],
+                 redaction: str, message:
+                 str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(field + '=.*?' + separator,
+                         field + '=' + redaction + separator, message)
+    return message
