ace36b66c374ce6bde91514cac282c2ffa84923d
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-20 22:41:10 -0500

    Add: auth

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Authentication module
+"""
+from flask import flask, request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ Auth Class
+    """
+
+    # fix both with slash and without slash
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Requre auth
+        """
+        if path is None or excluded_paths is None:
+            return True
+        if len(excluded_paths) == 0:
+            return True
+        if path not in excluded_paths:
+            return True
+        else:
+            return False
+    
+    def authorization_header(self, request=None) -> str:
+        """ Authorization_header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
