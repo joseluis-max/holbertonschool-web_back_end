529a1971fb173ee0a2dd5204d8f0e60aa9134835
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-21 20:48:31 -0500

    Add: erros

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -4,11 +4,11 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from api.v1.auth import Auth
-from api.v1.auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -16,10 +16,17 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-if getenv('AUTH_TYPE') == 'basic_auth':
+
+if os.getenv('AUTH_TYPE') == 'basic_auth':
     auth = BasicAuth()
 else:
     auth = Auth()
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """unauthorized handler"""
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(404)
@@ -29,27 +36,22 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
 @app.errorhandler(403)
-def not_found(error) -> str:
-    """ Forbidden
-    """
+def forbiden(error) -> str:
+    """forbiden handler"""
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def before_request() -> None:
+def before_req() -> None:
     """method to handler before_request"""
-    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if auth is None:
-        return None
-    if not auth.requre_auth(request.path, paths):
+        return
+    req_list = ['/api/v1/status/',
+                '/api/v1/unauthorized/',
+                '/api/v1/forbidden/']
+
+    if not auth.require_auth(request.path, req_list):
         return
     if auth.authorization_header(request) is None:
         abort(401)
