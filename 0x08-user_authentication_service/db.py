5cf4cfba79a55ba7bce0c68657437d41126a2fc4
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-27 21:37:14 -0500

    Add: query a user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base
 from user import User
@@ -36,3 +38,13 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Find a user by arbitrary keyword """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except InvalidRequestError:
+            raise
