3002ab4e6968651f85336bd57e3b051d9536e9cc
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-29 14:26:59 -0500

    Add: reset token

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -62,6 +62,16 @@ class Auth:
         self._db.update_user(user_id, session_id=None)
         return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """ Find the user corresponding to the email. """
+        try:
+            user = self._db.find_user_by(email=email)
+            token: str = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except NoResultFound:
+            raise ValueError
+
 
 def _hash_password(password: str) -> bytes:
     """ salted hash of the input password, hashed with bcrypt.hashpw. """
