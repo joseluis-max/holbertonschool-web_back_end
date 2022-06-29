d0c9b83edc66b22476a447f4f7a29e3a057c1280
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-29 11:14:53 -0500

    Add: valid login

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -2,6 +2,8 @@
 """ define a _hash_password method that takes
     in a password string arguments and returns bytes.
 """
+import bcrypt
+
 from db import DB
 from user import User
 from sqlalchemy.exc import NoResultFound
@@ -20,7 +22,15 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError("User {} already exists".format(email))
         except NoResultFound:
-            return self._db.add_user(email, str(_hash_password(password)))
+            return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Credentials validation """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
 
 
 def _hash_password(password: str) -> bytes:
