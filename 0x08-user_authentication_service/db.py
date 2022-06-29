3ed5afd41f8d2fd02f64524e700fddbf5aa31a3c
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-29 15:01:28 -0500

    Update: db.py pycodestyle

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,7 +1,6 @@
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
