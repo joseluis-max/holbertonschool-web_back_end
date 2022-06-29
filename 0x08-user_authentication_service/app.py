1e4c28365a73a3cefc2914ff92dca4eeeb44f2a4
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-29 10:28:40 -0500

    Add: user register from flask app

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
 """ Flask app """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
 @app.route('/', method=["GET"])
 def home():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/user', method=["POST"])
+def user():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": {}, "message": "user created".format(email)}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
