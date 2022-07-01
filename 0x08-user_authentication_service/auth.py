b8ad32ff87f8e84c2f8fa3749d40c769b7ec65cc
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-07-01 11:29:07 -0500

    Add: _hash_password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -5,7 +5,7 @@
 import bcrypt
 from db import DB
 from user import User
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
 from typing import Union
 
