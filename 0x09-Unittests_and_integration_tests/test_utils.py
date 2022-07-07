93be71adfe2387f91dc9f85c8723a0f17064cf79
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-07-06 20:29:26 -0500

    Update: pycodestyle

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -15,7 +15,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any):
+    def test_access_nested_map(self,
+                               nested_map: Mapping,
+                               path: Sequence, expected: Any):
         """ Cases access nested map """
         self.assertEqual(access_nested_map(nested_map, path), expected)
