90dfce0dfb6ca022da538167e60e6d9730541694
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-07-06 20:27:41 -0500

    Add: test nested map

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" unit test for utils.access_nested_map
+"""
+from typing import Any, Mapping, Sequence
+import unittest
+from parameterized import parameterized, parameterized_class
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Testing cases for utils.access_nested_map """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any):
+        """ Cases access nested map """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
