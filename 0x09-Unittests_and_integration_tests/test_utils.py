d6f444369447ccb98bec38ead216cb9c9006f417
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-07-06 20:36:53 -0500

    Add: Exception test

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -20,3 +20,15 @@ class TestAccessNestedMap(unittest.TestCase):
                                path: Sequence, expected: Any):
         """ Cases access nested map """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError)
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self,
+                                         nested_map: Mapping,
+                                         path: Sequence,
+                                         expected: Exception):
+        """ Test access nested map exception """
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
