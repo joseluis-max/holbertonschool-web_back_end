d87b4ae5dd9aaf5d683fd0c918942f7e48c71305
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-07-28 22:17:30 -0500

    Add: object  method properties

--- 0x0E-ES6_basic/9-getFullBudget.js
+++ 0x0E-ES6_basic/9-getFullBudget.js
@@ -7,7 +7,7 @@ export default function getFullBudgetObject(income, gdp, capita) {
     getIncomeInDollars(income) {
       return `$${income}`;
     },
-    getIncomeInEuros (income) {
+    getIncomeInEuros(income) {
       return `${income} euros`;
     },
   };
