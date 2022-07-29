16d0ae22b3b17348603a174e2312537b9a9777e3
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-07-28 22:13:12 -0500

    Add: compute name object

--- 0x0E-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x0E-ES6_basic/8-getBudgetCurrentYear.js
@@ -6,8 +6,8 @@ function getCurrentYear() {
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
     [`income-${getCurrentYear()}`]: income,
-    [`gdp-${getCurrentYear()}`] : gdp,
-    [`capita-${getCurrentYear()}`] : capita,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
   };
 
   return budget;
