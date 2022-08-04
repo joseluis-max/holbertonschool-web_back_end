fab63dea8dab136991874e3429ac8c35bdfdd2bb
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-08-03 20:54:21 -0500

    Add: class and getters and setters

---
+++ 0x10-ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+/**
+ * ClassRoom Abstraction
+ * @class
+ */
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class HolbertonCourse{
+  constructor(name, length, students){
+    if (typeof name !== 'string') throw new TypeError("Name must be a string");
+    if (typeof length !== 'number') throw new TypeError("Length must be a number");
+    if (!Array.isArray(students)) throw new TypeError("Students must be a array");
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+  
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') throw new TypeError("Name must be a string");
+    this._name = name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') throw new TypeError("Length must be a number");
+    this._length = length;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) throw new TypeError("Students must be a array");
+    this._students = students;
+  }
+}
