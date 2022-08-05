ee40d05c7478a33f42a0acb813657514babec057
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-08-04 21:46:29 -0500

    Add: changes eslint

--- 0x10-ES6_classes/1-make_classrooms.js
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,11 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom.js';
 
-export default function initialzeRooms() {
+function initialzeRooms() {
   return [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34)
   ];
 }
+
+export default initialzeRooms;
