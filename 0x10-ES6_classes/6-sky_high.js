1223840333bdff441c2a185561ea2c5aa2a2b9e6
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-08-04 22:04:33 -0500

    Add: changes eslint

--- 0x10-ES6_classes/6-sky_high.js
+++ 0x10-ES6_classes/6-sky_high.js
@@ -1,4 +1,4 @@
-import Building from "./5-building";
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
@@ -9,12 +9,12 @@ export default class SkyHighBuilding extends Building {
   get floors() {
     return this._floors;
   }
-  
+
   set floors(floors) {
     this._floors = floors;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
