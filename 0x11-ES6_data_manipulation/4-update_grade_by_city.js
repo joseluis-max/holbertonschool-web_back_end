db60c00d26d1234e38e4f43acc6b358f174d43f1
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-08-10 21:14:58 -0500

    Add: readme

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,15 @@
-import getStudentsByLocation from "./2-get_students_by_loc"
+import getStudentsByLocation from './2-get_students_by_loc';
 
-export default function updateStudentGradeByCity(listStudents, city,  newGrades) {
+export default function updateStudentGradeByCity(listStudents, city, newGrades) {
   const list = getStudentsByLocation(listStudents, city);
-  return list.map(student => {
-    const list = newGrades.filter(grade => grade.studentId === student.id);
+  return list.map((student) => {
+    const list = newGrades.filter((grade) => grade.studentId === student.id);
+    let obj = {};
     if (list.length > 0) {
-      student.grade = list[0].grade;
+      obj = { ...student, grade: list[0].grade };
     } else {
-      student.grade = "N/A";
+      obj = { ...student, grade: 'N/A' };
     }
-    return student;
+    return obj;
   });
 }
