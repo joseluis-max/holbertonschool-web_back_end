63f0c87cf6d87e25390ed6ae8cee21a50d8d1f08
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-08-10 21:44:27 -0500

    Add: set

--- 0x11-ES6_data_manipulation/7-has_array_values.js
+++ 0x11-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,8 @@
 export default function hasValuesFromArray(set, array) {
-  return set.hasValuesFromArray(array);
+  for (const elemet of array) {
+    if (set.has(elemet) === false) {
+      return false;
+    }
+  }
+  return true;
 }
