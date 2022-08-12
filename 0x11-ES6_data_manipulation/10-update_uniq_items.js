bafcb21be0ce5669f68e4fcf97695bc4464f96c9
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-08-11 21:11:46 -0500

    Add: Map

--- 0x11-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,5 @@
 export default function updateUniqueItems(map) {
-  if (map instanceof Map){
+  if (map instanceof Map) {
     map.forEach((value, key) => {
       if (value === 1) {
         map.set(key, 100);
