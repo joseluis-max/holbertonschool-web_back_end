7a16694dfdf20c3cbe104bf509e02e7150814287
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-08-24 21:19:54 -0500

    Add: redis op

---
+++ 0x14-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,26 @@
+import { createClient, print } from "redis";
+
+const client = createClient();
+ 
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+client.on('connect', (err) => {
+  console.error('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) throw err;
+    console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
