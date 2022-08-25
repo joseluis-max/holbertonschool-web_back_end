ca4c53c132a786134639d1bf4fd1f9dfe6135469
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-08-24 20:49:13 -0500

    Add: redis client

---
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+import { createClient } from "redis";
+
+const client = createClient();
+ 
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+client.on('connect', (err) => {
+  console.error('Redis client connected to the server');
+});
